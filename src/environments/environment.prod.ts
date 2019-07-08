51590335b0ad1a2d89a7e43195b26c63f3b9837d
4salistair <4salistair@gmail.com>
2019-07-08 12:55:11 +0100

    re add environ, queak to gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+
+    firebase: {
+      apiKey: 'AIzaSyA267O70xvE2dYfwPQysb_IBoGGLzt0HfY',
+      authDomain: 'ngfitnesstrackeer.firebaseapp.com',
+      databaseURL: 'https://ngfitnesstrackeer.firebaseio.com',
+      projectId: 'ngfitnesstrackeer',
+      storageBucket: 'ngfitnesstrackeer.appspot.com',
+      messagingSenderId: '363582832781'
+    }
+  };
+
